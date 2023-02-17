a71c96f3426976305ec015d236496e53b91fb1de
meghakulkarni1217 <124066137+meghakulkarni1217@users.noreply.github.com>
2023-02-17 20:28:24 +0530

    Swapping value
    Swapping value

---
+++ 02_swapVariables.js
@@ -0,0 +1,37 @@
+var num1=100;//200
+var num2=200;//100
+var num3=300;
+console.log("Swapping before value");
+console.log(num1,num2);
+var temp=num1;
+num1=num2;
+num2=temp;
+console.log("Swapping after value");
+console.log(num1,num2);
+
+console.log("Swapping before value");
+console.log(num2,num3);
+var temp1=num2;
+num2=num3;
+num3=temp1;
+console.log("Swapping after value");
+console.log(num2,num3);
+
+console.log("Swapping before value");
+console.log(num3,num1);
+var temp2=num3;
+num3=num1;
+num1=temp2;
+console.log("Swapping after value");
+console.log(num3,num1);
+
+console.log("***Second Program***");
+var name1="Sweety";
+var name2="Cutie";
+console.log("Swapping before value");
+console.log(name1,name2);
+var temp3=name1;
+name1=name2;
+name2=temp3;
+console.log("Swapping after value");
+console.log(name1,name2);
