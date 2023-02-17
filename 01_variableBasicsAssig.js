de3bde1a550dc65b90909ddc9a832919c5af1a23
meghakulkarni1217 <124066137+meghakulkarni1217@users.noreply.github.com>
2023-02-17 14:29:22 +0530

    Assignment Complete
    Assignment Complete

---
+++ 01_variableBasicsAssig.js
@@ -0,0 +1,21 @@
+var myName = 'Virat Kohli';
+var age = 31;
+var isMarried = true;
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+console.log("my fullName is:",myName);
+
+console.log("typeof age is:",typeof age);
+console.log("my age is:",age);
+
+console.log("typeof age is:",isMarried);
+
+var city=null;
+console.log("value of city variable is:",city);
+console.log("typeof city is:",typeof city);
+
+//  var university;
+// university="Solapur";
+//  console.log("university name is:",Solapur);
+
+
---
+++ 02_typeOf.js
@@ -0,0 +1,17 @@
+var bankName="SBI Bank";
+console.log("bank name is:",bankName);
+console.log("typeof bank is:",typeof bankName);
+
+var marks="90%";
+console.log("Marks is:",90);
+console.log("typeof mark is:",typeof marks);
+
+var isWorking=true;
+console.log("typeof isWorking:",typeof isWorking);
+
+var totalCount="Hundred and Seven";
+console.log("totalCount:",totalCount);
+console.log("typeof totalCount:", typeof totalCount);
+var totalCount=107;
+console.log("Update value of:",totalCount);
+console.log("typeof totalCount:",typeof totalCount);
