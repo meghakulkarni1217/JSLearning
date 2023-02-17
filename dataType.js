f510ac64110db3360f42ff9d2806cfcc446ebc18
meghakulkarni1217 <124066137+meghakulkarni1217@users.noreply.github.com>
2023-02-17 13:03:08 +0530

    Assign completed.
    Assign completed.

---
+++ dataType.js
@@ -0,0 +1,39 @@
+var myName = 'Virat Kohli';
+var fatherName = "Yogesh Kohli"
+
+var age = 31;
+var weight = 82.5;
+
+var isMarried = true;
+var isLazy = false; 
+
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+console.log("typeOf variable fatherName:", typeof fatherName);
+console.log("typeof age is:",typeof age);
+console.log("typeof variable weight:",typeof weight);
+
+
+console.log("typeof age is:",isMarried);
+console.log("typeof weight is:",isLazy);
+
+
+var graduation;
+console.log("Value of  graduation variable is: ", graduation);
+console.log("typeOf graduation is: ", graduation);
+
+var city=null;
+console.log("value of city variable is:",city);
+console.log("typeof city is:",typeof city);
+
+age="32";
+console.log(typeof age);
+
+console.log("Swapping variables");
+var num1 = 100; // 200
+var num2 = 200; // 100
+
+var temp = num1;
+num1 = num2; 
+num2 = temp;
+console.log(num1, num2);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,23 @@
 console.log("Mohit Sharma");
+console.log(31);
+console.log("pune");
+
+var fullName="Mohit Sharma";
+console.log(fullName);
+console.log(fullName);
+
+var city;//variable  Declaration
+console.log(city);
+city="pune";//variable initialization
+console.log(city);
+city="Surat";//variable Update
+console.log(city);
+
+var age=31; //variable declaration and initialization in same line
+age=32;
+age=33;
+console.log(age);
+
+var university;// variable Declare
+university="SGBAU";//variable initialization
+console.log(universityName);
