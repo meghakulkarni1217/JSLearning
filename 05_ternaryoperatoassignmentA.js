06115b466feb0d5248a068726344dab0f8697da7
meghakulkarni1217 <124066137+meghakulkarni1217@users.noreply.github.com>
2023-02-23 20:29:44 +0530

    ternary assignmrnt complete
    ternary assignmrnt complete

--- 05_ternaryoperatoassignmentA.js
+++ 05_ternaryoperatoassignmentA.js
@@ -1,22 +1,58 @@
-console.log(`=====Find the greatest number amongst two number:=====`);
-
-console.log(`==Step 2==`);
-function isEvenOrOddNum(){
-var isEvenOrOddNum=29;
+var num1=10;
+var num2=-10;
+function greaterNumber(){
+result=num1>=num2;
+return greaterNumber;
 }
-var result=isEvenOrOddNum%2==0?"${IsEven}":"Odd";
- console.log(result);
+ var greatervalue=greaterNumber(num1,num2);
+console.log(`The given no 10 is more than -10`);
+console.log(result);
 
+var num1=800;
+var num2=899;
+function greaterNumber(){
+result=num1>=num2;
+return greaterNumber;
+}
+ var greatervalue=greaterNumber(num1,num2);
+console.log(`The given no 800 is  not more than 899`);
+console.log(result);
+  console.log(`===Step2===`);
+var num=29;
+function isEvenOrOdd(){
+    result= num%2==0?"Even":"Odd";
+    return isEvenOrOdd;
+}
+var EvenOdd=isEvenOrOdd(num);
+console.log(`The given no is 29`);
+console.log(result);
 
+var num=44;
+function isEvenOrOdd(){
+    result= num%2==0?"Even":"Odd";
+    return isEvenOrOdd;
+}
+var EvenOdd=isEvenOrOdd(num);
+console.log(`The given no is 44`);
+console.log(result);
 
+var num=0;
+function isEvenOrOdd(){
+    result= num%2==0?"Even":"Odd";
+    return isEvenOrOdd;
+}
+var EvenOdd=isEvenOrOdd(num);
+console.log(`The given no is 0`);
+console.log(result);
 
-
-
-
-
-
-
-
+var num=101;
+function isEvenOrOdd(){
+    result= num%2==0?"Even":"Odd";
+    return isEvenOrOdd;
+}
+var EvenOdd=isEvenOrOdd(num);
+console.log(`The given no is 101 `);
+console.log(result);
 
 console.log(`==Step 3==`);
 function wordLength(word){
