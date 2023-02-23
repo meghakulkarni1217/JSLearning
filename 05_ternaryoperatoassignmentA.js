dbd503ce0cca52172e6bc96334976894eb56d44a
meghakulkarni1217 <124066137+meghakulkarni1217@users.noreply.github.com>
2023-02-23 13:59:46 +0530

    rename
    rename

---
+++ 05_ternaryoperatoassignmentA.js
@@ -0,0 +1,31 @@
+console.log(`=====Find the greatest number amongst two number:=====`);
+
+console.log(`==Step 2==`);
+function isEvenOrOddNum(){
+var isEvenOrOddNum=29;
+}
+var result=isEvenOrOddNum%2==0?"${IsEven}":"Odd";
+ console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(`==Step 3==`);
+function wordLength(word){
+    var wordLength = word.length;
+    var result = wordLength%2==0 ? "EVEN" : "ODD";
+    return result;
+ }
+ var result = wordLength("JavaScript");
+ console.log(`Word "JavaScript" has ${result} length` );
+ console.log(`Word "Google" has ${wordLength("Google")} length` );
+ var result = wordLength("Developer");
+ console.log(`Word "Developer" has ${wordLength("Developer")} length` );
--- assignmentA.js
+++
@@ -1,8 +0,0 @@
-console.log(`=====Find the greatest number amongst two number:=====`);
-function greaterNumber(num1,num2){
-
-    var num1=10;
-    var num2=-10;
-   var result=num1>=num2?greater:smaller;
-    console.log(`The greater number is:,result`);
-}
