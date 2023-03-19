ebffcc042f5e769adf8d20a63c75628e6621c1a9
meghakulkarni1217 <124066137+meghakulkarni1217@users.noreply.github.com>
2023-03-19 21:50:28 +0530

    Add files via upload

---
+++ 14_callbacks.js
@@ -0,0 +1,10 @@
+function doAssignment(callback){
+    console.log("Solving Assignments");
+    console.log("step 1 to 10 all ares solved");
+    callback();
+}
+function copyAssignments(){
+    console.log("Thank you let copy assignmens");
+    console.log(`ohh finally copied assignment`);
+}
+doAssignment(copyAssignments);
