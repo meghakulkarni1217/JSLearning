877ed2c7ba2a89bbddc56f3036a20be515cee522
Kaustubh Kulkarni <124066137+meghakulkarni1217@users.noreply.github.com>
2023-03-21 16:47:37 +0530

    code complete
    code complete

--- 15_AssignB.js
+++ 15_AssignB.js
@@ -60,4 +60,23 @@ for (const Employee of Emp) {
     
 }
 
+//ForEach Use
+const arrayEmployee = [empAnil, empRadha, empRishi, empSonali, empMonika, empViny, empMahi];
 
+console.log("Find the employees whose name start with R....");
+arrayEmployee.forEach(employee => {
+    let empName = employee.empName;
+    if (empName.startsWith("R")) {
+        console.log(employee);
+    }
+});
+
+
+console.log("Infy employees total salary");
+let totalSalaryOfInfy = 0;
+arrayEmployee.forEach(employee => {
+    if(employee.empCompany=="Infy") {
+        totalSalaryOfInfy = totalSalaryOfInfy + employee.empSalary; 
+    }
+});
+console.log(totalSalaryOfInfy);
